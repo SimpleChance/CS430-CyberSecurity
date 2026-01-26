58090ccc5cbf4ffa2579fd205fe7383a05a93342
SimpleChance <cwjewell19@gmail.com>
2026-01-26 02:59:08 -0500

    Full encryption + some fixes

--- Assignment1/cryptomatic.py
+++ Assignment1/cryptomatic.py
@@ -59,12 +59,12 @@ def ascii_to_str(x: [int]) -> str:
             y: str
     """
     y = "".join(chr(ascii_code) for ascii_code in x)  # convert each ascii code in x to char and join to a string
-    return ""
+    return y
 
 
-def encrypt(x: [str], y: [str]) -> [str]:
+def encryptdecrypt(x: [str], y: [str]) -> [str]:
     """
-        Encrypts a message (in binary list form) using a 2-character key.
+        Encrypts and decrypts a message (in binary list form) using a 2-character key.
 
         Args:
             x: [str]
@@ -76,13 +76,60 @@ def encrypt(x: [str], y: [str]) -> [str]:
 
     # XOR two equal length lists of 8-bit binary strings
     z = [
-        format(int(a, 2) ^ int(b, 2), f"0{len(a)}b")
+        format(int(a, 2) ^ int(b, 2), "08b")
         for a, b in zip(x, y)
     ]
+    return z
 
 
 def main():
-    return
+    print("Assignment1 - Crpytomatic \n \n")
+    print("Please Enter 1, 2, or 3 to make a selection:\n")
+    print("1. Encrypt \n2. Decrypt \n3. Bruteforce")
+
+    i = int(input())
+
+    match i:
+        case 1:
+            print("\nEnter a file name and 2-character code separated by a space. (i.e. input.txt ab")
+            filename, key = input().split()
+
+            print("\nfilename:", filename)
+            print("key:", key)
+
+            with open(filename, "r", encoding="utf-8") as file:
+                input_text = file.read()
+
+            # If length of input text is odd, pad with a space.
+            k = len(input_text)
+            if k % 2 != 0:
+                input_text += " "
+                k += 1
+            print("k:", k)
+
+            full_length_key = key * (k//2)  # repeating key string of length k
+
+            binary_text = ascii_to_bin(str_to_ascii(input_text))  # convert input text to array of 8-bit binary strings
+            binary_key = ascii_to_bin(str_to_ascii(full_length_key))  # convert key to array of 8-bit strings
+
+            encrypted_binary_text = encryptdecrypt(binary_text, binary_key)
+            encrypted_text = ascii_to_str(bin_to_ascii(encrypted_binary_text))  # convert 8-bit binary array to string
+
+            print("\nEncrypted message:\n")
+            print(encrypted_text)
+
+            print("\nEnter an output filename:")
+            outputfile = input()
+            with open("Encrypted/" + outputfile, "w", encoding="utf-8") as f:
+                f.write(encrypted_text)
+            print("Saved encrypted message to ", "Encrypted/" + outputfile)
+
+        case 2:
+            return
+        case 3:
+            return
+        case _:
+            return
 
 
 if __name__ == "__main__":
