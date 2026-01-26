72fd2acaef102eafaef76d02bf63c102cfa68673
SimpleChance <cwjewell19@gmail.com>
2026-01-25 22:52:42 -0500

    Setup

---
+++ Assignment1/cryptomatic.py
@@ -0,0 +1,66 @@
+"""
+cryptomatic.py
+
+Encryption and Decryption scheme using a 2-character key.
+Bruteforce algorithm to generate a 2-character key given an encrypted message.
+"""
+
+
+def str_to_ascii(x: str) -> int:
+    """
+        Convert string to ascii.
+
+        Args:
+            x: str
+
+        Returns:
+            y: int
+    """
+    return 0
+
+
+def ascii_to_bin(x: int) -> int:
+    """
+        Convert ascii to binary.
+
+        Args:
+            x: int
+
+        Returns:
+            y: int
+    """
+    return 0
+
+
+def bin_to_ascii(x: int) -> int:
+    """
+        Convert binary to ascii.
+
+        Args:
+            x: int
+
+        Returns:
+            y: int
+    """
+    return 0
+
+
+def ascii_to_str(x: int) -> str:
+    """
+        Convert ascii to string.
+
+        Args:
+            x: int
+
+        Returns:
+            y: int
+    """
+    return ""
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
