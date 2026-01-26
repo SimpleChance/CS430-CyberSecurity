3d4d618c838ad83d2d2af927c690f1df9903245f
SimpleChance <cwjewell19@gmail.com>
2026-01-25 23:29:17 -0500

    Encrypt method

--- Assignment1/cryptomatic.py
+++ Assignment1/cryptomatic.py
@@ -6,7 +6,7 @@ Bruteforce algorithm to generate a 2-character key given an encrypted message.
 """
 
 
-def str_to_ascii(x: str) -> int:
+def str_to_ascii(x: str) -> [int]:
     """
         Convert string to ascii.
 
@@ -14,48 +14,71 @@ def str_to_ascii(x: str) -> int:
             x: str
 
         Returns:
-            y: int
+            y: [int]
     """
-    return 0
+    y = [ord(_) for _ in x]  # convert each character in x to ascii
+    return y
 
 
-def ascii_to_bin(x: int) -> int:
+def ascii_to_bin(x: [int]) -> [str]:
     """
         Convert ascii to binary.
 
         Args:
-            x: int
+            x: [int]
 
         Returns:
-            y: int
+            y: [str]
     """
-    return 0
+    y = [format(ascii_code, "08b") for ascii_code in x]  # convert each ascii code in x to 8-bit binary str
+    return y
 
 
-def bin_to_ascii(x: int) -> int:
+def bin_to_ascii(x: [str]) -> [int]:
     """
         Convert binary to ascii.
 
         Args:
-            x: int
+            x: [str]
 
         Returns:
-            y: int
+            y: [int]
     """
-    return 0
+    y = [int(binary, 2) for binary in x]  # convert each binary string in x to ascii code
+    return y
 
 
-def ascii_to_str(x: int) -> str:
+def ascii_to_str(x: [int]) -> str:
     """
         Convert ascii to string.
 
         Args:
-            x: int
+            x: [int]
 
         Returns:
-            y: int
+            y: str
     """
+    y = "".join(chr(ascii_code) for ascii_code in x)  # convert each ascii code in x to char and join to a string
     return ""
+
+
+def encrypt(x: [str], y: [str]) -> [str]:
+    """
+        Encrypts a message (in binary list form) using a 2-character key.
+
+        Args:
+            x: [str]
+            y: [str]
+
+        Returns:
+            z: [str]
+    """
+
+    # XOR two equal length lists of 8-bit binary strings
+    z = [
+        format(int(a, 2) ^ int(b, 2), f"0{len(a)}b")
+        for a, b in zip(x, y)
+    ]
 
 
 def main():
